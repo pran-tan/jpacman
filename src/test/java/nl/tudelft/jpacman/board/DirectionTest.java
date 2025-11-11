c64212631ccd76bdaf1c1d7b46c7db9e4acf2dcf
Pran Tantipiwatanaskul <pran.tan@student.mahidol.edu>
2025-11-11 17:40:25 +0700

    Add new test cases for Direction (NORTH, SOUTH, EAST, WEST)

--- src/test/java/nl/tudelft/jpacman/board/DirectionTest.java
+++ src/test/java/nl/tudelft/jpacman/board/DirectionTest.java
@@ -1,22 +1,38 @@
 package nl.tudelft.jpacman.board;
 
 import static org.assertj.core.api.Assertions.assertThat;
-
 import org.junit.jupiter.api.Test;
 
 /**
- * A very simple (and not particularly useful)
- * test class to have a starting point where to put tests.
- *
- * @author Arie van Deursen
+ * Tests the Direction enum for correct delta values.
  */
 public class DirectionTest {
-    /**
-     * Do we get the correct delta when moving north?
-     */
+
     @Test
     void testNorth() {
         Direction north = Direction.valueOf("NORTH");
+        assertThat(north.getDeltaX()).isEqualTo(0);
         assertThat(north.getDeltaY()).isEqualTo(-1);
+    }
+
+    @Test
+    void testSouth() {
+        Direction south = Direction.valueOf("SOUTH");
+        assertThat(south.getDeltaX()).isEqualTo(0);
+        assertThat(south.getDeltaY()).isEqualTo(1);
+    }
+
+    @Test
+    void testEast() {
+        Direction east = Direction.valueOf("EAST");
+        assertThat(east.getDeltaX()).isEqualTo(1);
+        assertThat(east.getDeltaY()).isEqualTo(0);
+    }
+
+    @Test
+    void testWest() {
+        Direction west = Direction.valueOf("WEST");
+        assertThat(west.getDeltaX()).isEqualTo(-1);
+        assertThat(west.getDeltaY()).isEqualTo(0);
     }
 }
